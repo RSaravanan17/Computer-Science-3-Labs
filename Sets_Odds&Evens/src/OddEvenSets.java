81bef752a43c4eb9a9529d42416eb870a11c48dc
RSaravanan17 <rithvik.saravanan17@gmail.com>
2017-09-24 23:08:32 -0500

    Sets - Odds & Evens

--- Sets_Odds&Evens/src/OddEvenSets.java
+++ Sets_Odds&Evens/src/OddEvenSets.java
@@ -6,15 +6,17 @@
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 import static java.lang.System.*;
 
 public class OddEvenSets
 {
-	private Set<String> values;
-	private Set<String> odds;
-	private Set<String> evens;
-	private Set<String> perfects;
+	private Set<Integer> values;
+	private Set<Integer> odds;
+	private Set<Integer> evens;
+	private Set<Integer> perfects;
 
 	public OddEvenSets()
 	{
@@ -22,29 +24,33 @@ public class OddEvenSets
 
 	public OddEvenSets(String line)
 	{
-		values = new TreeSet<String>(Arrays.asList((line.split(" "))));
+		odds = new TreeSet<Integer>();
+		evens = new TreeSet<Integer>();
+		perfects = new TreeSet<Integer>();
+		List<String> temp = new ArrayList<String>(Arrays.asList(line.split(" ")));
+		List<Integer> temp1 = new ArrayList<Integer>();
+		for (int i = 0; i < temp.size(); i++) {
+			temp1.add(Integer.parseInt(temp.get(i)));
+		}
+		values = new TreeSet<Integer>(temp1);
 	}
 
 	public void sortSets()
 	{
-		odds = new TreeSet<String>();
-		evens = new TreeSet<String>();
-		perfects = new TreeSet<String>();
-
-		for (String num : values) {
+		for (int num : values) {
 			int sum = 0;
 
-			if (Integer.parseInt(num) % 2 == 0) {
+			if (num % 2 == 0) {
 				evens.add(num);
-			} else if (Integer.parseInt(num) % 2 == 1) {
+			} else if (num % 2 == 1) {
 				odds.add(num);
 			}
-			for (int i = 1; i < Integer.parseInt(num); i++) {
-				if (Integer.parseInt(num) % i == 0) {
+			for (int i = 1; i < num; i++) {
+				if (num % i == 0) {
 					sum += i;
 				}
 			}
-			if (sum == Integer.parseInt(num)) {
+			if (sum == num) {
 				perfects.add(num);
 			}
 		}
